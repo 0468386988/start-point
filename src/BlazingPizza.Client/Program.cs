315223749d4d1374091c4a3d1130c6fe803c0dea
0468386988 <51746409+0468386988@users.noreply.github.com>
2020-05-01 22:48:12 +1000

    Revert "Wrong start point version"
    This reverts commit aca481e41c8c469bfc93b8fd27d510c2244837ec.

--- src/BlazingPizza.Client/Program.cs
+++ src/BlazingPizza.Client/Program.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.AspNetCore.Blazor.Hosting;
+﻿using Microsoft.AspNetCore.Components.Authorization;
+using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.Extensions.DependencyInjection;
 using System.Threading.Tasks;
 
 namespace BlazingPizza.Client
@@ -8,7 +10,17 @@ namespace BlazingPizza.Client
         public static async Task Main(string[] args)
         {
             var builder = WebAssemblyHostBuilder.CreateDefault(args);
+
             builder.RootComponents.Add<App>("app");
+
+            builder.Services.AddScoped<OrderState>();
+
+            // Add auth services
+            builder.Services.AddOptions();
+            builder.Services.AddAuthorizationCore();
+            builder.Services.AddScoped<AuthenticationStateProvider, ServerAuthenticationStateProvider>();
+
+            builder.Services.AddBaseAddressHttpClient();
 
             await builder.Build().RunAsync();
         }
